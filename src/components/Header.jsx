a60ef350e296a361606e264dde5bbdd1621048c9
STEPHANIE G JOHNSON <STEPHANIEJOHNSON.ESQ@GMAIL.COM>
2023-12-08 14:35:25 -0500

    added animation to header subtitle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function Header(props) {
   return (
     <header className="header">
-      <p className = "header-text">Explore The City</p>
-      <p className="header-sub">Maps. Weather. Movies.</p>
+      <p className="header-text">Explore The City</p>
+      <p className="header-sub bounce">Maps. Weather. Movies.</p> 
     </header>
-  )
+  );
 }
 
 export default Header;
